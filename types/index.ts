3122cb81ddc705ae39452c436b6b8c1bdb9b0a82
John Amores <johnamores@Johns-MacBook-Pro.local>
2024-06-11 18:49:19 -0400

    Added search and filter functionality

--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,8 @@ export interface CustomButtonProps {
   containerStyles?: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
+
+export interface SearchManuFacturerProps {
+  manufacturer: string;
+  setManuFacturer: (manufacturer: string) => void;
+}
